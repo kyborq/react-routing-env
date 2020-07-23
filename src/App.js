d00604e89bd514047c5e78edb611d0112b67986e
Kyborq <k.podyganov@mail.ru>
2020-07-23 17:50:04 +0300

    Cleaned App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from "react";
+import { Router as BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { HomePage } from './pages/HomePage';
-import { AboutPage } from './pages/AboutPage';
+import { HomePage } from "./pages/HomePage";
+import { AboutPage } from "./pages/AboutPage";
 
-import './styles.css';
+import "./styles.css";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/about" component={AboutPage} />
       </Switch>
-    </BrowserRouter>
+    </Router>
   );
 }
