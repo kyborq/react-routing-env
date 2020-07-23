3c09ed6452e077946ea4f492744bef84bedbe36e
Konstantin <52314985+kyborq@users.noreply.github.com>
2020-07-23 17:51:33 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router as BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router , Route, Switch } from "react-router-dom";
 
 import { HomePage } from "./pages/HomePage";
 import { AboutPage } from "./pages/AboutPage";
